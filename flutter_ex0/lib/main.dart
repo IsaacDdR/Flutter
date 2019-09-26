c6d400466c2b5dcd881061bc56a4bde485445370
Isaacddr <isaacovic00@gmail.com>
2019-09-25 19:00:43 -0500

    flutter cards

--- flutter_ex0/lib/main.dart
+++ flutter_ex0/lib/main.dart
@@ -1,68 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/materia.dart';
 
+void main() => runApp(MyApp());
 
-class MyAppBar extends StatelessWidget {
+class MyApp extends StatelessWidget {
 
-  MyAppBar ({this.title});
-
-  final Widget title;
+  static const String _title = 'App';
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(
-       color: Colors.blue[500]
-      ),
-      child: Row(
-        children: <Widget> [
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation menu",
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget> [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          ),
-        ],
-      ),
-    );
-
-  }
-}
-
-void main(){
-  runApp(MaterialApp(
-    title: 'My app',
-    home: MyScaffold(),
-  ));
+  Widget
 }
